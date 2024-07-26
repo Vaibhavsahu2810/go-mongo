870f7fd10e048aa203f3b728b4f729304b29b2a3
Vaibhav <sahusv4527@gmail.com>
2024-07-26 15:04:33 +0530

    changes made

--- main.go
+++ main.go
@@ -1,8 +1,67 @@
 package main
 
-import(
+import (
+	"context"
+	"log"
+	"time"
 
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 type MongoInstance struct{
-	
+	Client	*mongo.Client
+	db		*mongo.Database
 }
+
+var mg MongoInstance
+const dbName = "mongo-go"
+const mongoUrl = "mongodb://localhost:27017"+ dbName
+
+type Employee struct{
+	ID		string	`json:"id,omitempty" bson:"_id,omitempty"`
+	Name	string	`json:"name"`
+	Salary	float64	`json:"salary"`
+	Age		float64	`json:"age"`
+}
+
+func Connect() error {
+    client, err := mongo.NewClient(options.Client().ApplyURI(mongoUrl))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    defer cancel()
+
+    err = client.Connect(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    Db := client.Database(dbName)
+    mg = MongoInstance{
+        Client: client,
+        db: Db,
+    }
+
+    return nil
+}
+
+func main(){
+	if err := Connect(); err != nil{
+		log.Fatal(err);
+	}
+	app := fiber.New()
+	app.Get("/employee" ,func(c *fiber.Ctx) error {
+		query = bson.D{{}}
+
+		Find(c.Context(),query)
+		var employees []Employee = make([Employee, 0])
+
+	} )
+}
+
+// sfksdfdfkdf
