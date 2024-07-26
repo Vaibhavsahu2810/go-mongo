064a33e46bb6c0f547b82a09d180f419dd93894e
Vaibhav <sahusv4527@gmail.com>
2024-07-26 18:33:51 +0530

    added routes

--- main.go
+++ main.go
@@ -6,62 +6,93 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-type MongoInstance struct{
-	Client	*mongo.Client
-	db		*mongo.Database
+
+type MongoInstance struct {
+	Client *mongo.Client
+	db     *mongo.Database
 }
 
 var mg MongoInstance
-const dbName = "mongo-go"
-const mongoUrl = "mongodb://localhost:27017"+ dbName
 
-type Employee struct{
-	ID		string	`json:"id,omitempty" bson:"_id,omitempty"`
-	Name	string	`json:"name"`
-	Salary	float64	`json:"salary"`
-	Age		float64	`json:"age"`
+const dbName = "mongo-go"
+const mongoUrl = "mongodb://localhost:27017" + dbName
+
+type Employee struct {
+	ID     string  `json:"id,omitempty" bson:"_id,omitempty"`
+	Name   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    float64 `json:"age"`
 }
 
 func Connect() error {
-    client, err := mongo.NewClient(options.Client().ApplyURI(mongoUrl))
-    if err != nil {
-        log.Fatal(err)
-    }
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoUrl))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-    err = client.Connect(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    Db := client.Database(dbName)
-    mg = MongoInstance{
-        Client: client,
-        db: Db,
-    }
+	Db := client.Database(dbName)
+	mg = MongoInstance{
+		Client: client,
+		db:     Db,
+	}
 
-    return nil
+	return nil
 }
 
-func main(){
-	if err := Connect(); err != nil{
-		log.Fatal(err);
+func main() {
+	if err := Connect(); err != nil {
+		log.Fatal(err)
 	}
 	app := fiber.New()
-	app.Get("/employee" ,func(c *fiber.Ctx) error {
-		query = bson.D{{}}
+	app.Get("/employee", func(c *fiber.Ctx) error {
 
-		Find(c.Context(),query)
-		var employees []Employee = make([Employee, 0])
+		query := bson.D{{}}
 
-	} )
+		cursor, err := mg.db.Collection("employees").Find(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		var employees []Employee = make([]Employee, 0)
+
+		if err := cursor.All(c.Context(), employees); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.JSON(employees)
+	})
+	app.Post("/employee", func(c *fiber.Ctx) error {
+
+		collection := mg.db.Collection("employees")
+		employee := new(Employee)
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		inResult, err := collection.InsertOne(c.Context(), employee)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		filter := bson.D{{Key: "_id", Value: inResult.InsertedID}}
+
+		createdRecord := collection.FindOne(c.Context(), filter)
+
+		createdEmployee := &Employee{}
+		if err := createdRecord.Decode(createdEmployee); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		return c.Status(201).JSON(createdEmployee)
+
+	})
 }
-
-// sfksdfdfkdf
